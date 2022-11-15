3cb00845c710d67bfd734be2db3970100f1494c1
wimblewamble <s3784709@student.rmit.edu.au>
2022-11-15 23:31:51 +1100

    Refactor

--- delivery_manager_app/lib/pages/delivery_manager.dart
+++ delivery_manager_app/lib/pages/delivery_manager.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_manager_app/assets/button_style.dart';
 import 'package:delivery_manager_app/assets/loading_spinner.dart';
+import 'package:delivery_manager_app/assets/menu_card.dart';
 import 'package:delivery_manager_app/classes/delivery_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
@@ -45,46 +46,5 @@ class DeliveryManager extends StatelessWidget {
             ),
           ],
         ));
-  }
-}
-
-class MenuCard extends StatelessWidget {
-  const MenuCard({
-    Key? key,
-    required this.title,
-    required this.description,
-    required this.button,
-  }) : super(key: key);
-
-  final String title;
-  final String description;
-  final Widget button;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        child: Container(
-      padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(description),
-          const SizedBox(
-            height: 8,
-          ),
-          Align(
-              alignment: Alignment.centerRight,
-              child: SizedBox(width: 125, child: button)),
-        ],
-      ),
-    ));
   }
 }
