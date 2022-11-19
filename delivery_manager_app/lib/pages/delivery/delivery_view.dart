8875a31cae902ecab54ae9ebbd352a10129b1fd2
wimblewamble <s3784709@student.rmit.edu.au>
2022-11-19 13:16:10 +1100

    Add commas for linting

--- delivery_manager_app/lib/pages/delivery/delivery_view.dart
+++ delivery_manager_app/lib/pages/delivery/delivery_view.dart
@@ -104,71 +104,76 @@ class _DeliveryViewState extends State<DeliveryView> {
         title: const Text('Schedule Deliveries'),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: FutureBuilder(
-            future: deliveryFuture,
-            builder: (context, snapshot) {
-              // If we're not done, then show the loading spinner.
-              if (snapshot.connectionState != ConnectionState.done) {
-                return Center(
-                  child: loadingSpinner(
-                    text: 'Loading appointments...',
-                    alignment: MainAxisAlignment.center,
-                  ),
-                );
-              }
-
-              // If an error occurred, display it.
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(snapshot.error.toString(),
-                      style: const TextStyle(
-                          color: Colors.red, fontWeight: FontWeight.bold)),
-                );
-              }
-
-              final allDeliveries = snapshot.requireData;
-
-              return TableCalendar(
-                firstDay: DateTime.now(),
-                lastDay: DateTime.now().add(const Duration(days: 365)),
-                focusedDay: _focusedDay,
-                calendarFormat: _calendarFormat,
-                selectedDayPredicate: (day) {
-                  // Use `selectedDayPredicate` to determine which day is currently selected.
-                  // If this returns true, then `day` will be marked as selected.
-
-                  // Using `isSameDay` is recommended to disregard
-                  // the time-part of compared DateTime objects.
-                  return isSameDay(_selectedDay, day);
-                },
-                onDaySelected: (selectedDay, focusedDay) {
-                  if (!isSameDay(_selectedDay, selectedDay)) {
-                    // Call `setState()` when updating the selected day
-                    setState(() {
-                      _selectedDay = selectedDay;
-                      _focusedDay = focusedDay;
-                    });
-                  }
-                },
-                onFormatChanged: (format) {
-                  if (_calendarFormat != format) {
-                    // Call `setState()` when updating calendar format
-                    setState(() {
-                      _calendarFormat = format;
-                    });
-                  }
-                },
-                onPageChanged: (focusedDay) {
-                  // No need to call `setState()` here
-                  _focusedDay = focusedDay;
-                },
-                eventLoader: (day) {
-                  return getDeliveriesForDay(day, allDeliveries);
-                },
+        padding: const EdgeInsets.all(8),
+        child: FutureBuilder(
+          future: deliveryFuture,
+          builder: (context, snapshot) {
+            // If we're not done, then show the loading spinner.
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(
+                child: loadingSpinner(
+                  text: 'Loading appointments...',
+                  alignment: MainAxisAlignment.center,
+                ),
               );
-            },
-          )),
+            }
+
+            // If an error occurred, display it.
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            }
+
+            final allDeliveries = snapshot.requireData;
+
+            return TableCalendar(
+              firstDay: DateTime.now(),
+              lastDay: DateTime.now().add(const Duration(days: 365)),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                // Use `selectedDayPredicate` to determine which day is currently selected.
+                // If this returns true, then `day` will be marked as selected.
+
+                // Using `isSameDay` is recommended to disregard
+                // the time-part of compared DateTime objects.
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  // Call `setState()` when updating the selected day
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  // Call `setState()` when updating calendar format
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                // No need to call `setState()` here
+                _focusedDay = focusedDay;
+              },
+              eventLoader: (day) {
+                return getDeliveriesForDay(day, allDeliveries);
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
