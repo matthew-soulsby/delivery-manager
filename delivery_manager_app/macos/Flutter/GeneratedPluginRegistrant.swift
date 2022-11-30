a4d63afc6306649ad2f2c2c32d0e1d2872f86b56
wimblewamble <s3784709@student.rmit.edu.au>
2022-11-30 14:05:14 +1100

    Add location

--- delivery_manager_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ delivery_manager_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import isar_flutter_libs
+import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
